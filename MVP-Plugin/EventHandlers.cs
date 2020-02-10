a19cf9b3ede47e449632cc88addb5213b9d7dcce
Luke-san <lukaszombiekiller@gmail.com>
2020-02-09 22:16:09 -0600

    Initial

---
+++ MVP-Plugin/EventHandlers.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using EXILED;
+using EXILED.Extensions;
+using UnityEngine;
+
+namespace MVP
+{
+    public class EventHandlers
+    {
+        public Plugin plugin;
+        public EventHandlers(Plugin plugin) => this.plugin = plugin;
+
+        public List<ReferenceHub> playersWithKills = new List<ReferenceHub>();
+        public List<int> playerKillNumber = new List<int>();
+
+        public void OnPlayerDeath(ref PlayerDeathEvent ev)
+        {
+            ReferenceHub killer = Player.GetPlayer(ev.Killer.GetNickname());
+            if (!playersWithKills.Contains(killer))
+            {
+                playersWithKills.Add(killer);
+                playerKillNumber.Add(0);
+            }
+            playerKillNumber[playersWithKills.IndexOf(killer)] += 1;
+        }
+
+        public void OnRoundEnd()
+        {
+            ReferenceHub mvp = findMVP();
+            foreach (GameObject ply in PlayerManager.players)
+            {
+                Player.GetPlayer(ply.name).Broadcast(10, $"{mvp.GetNickname()} was MVP of the match with {playerKillNumber[playersWithKills.IndexOf(mvp)].ToString()} kills!");
+            }
+            playersWithKills = new List<ReferenceHub>();
+            playerKillNumber = new List<int>();
+        }
+
+        public ReferenceHub findMVP()
+        {
+            int largestNumber = 0;
+            for (int i=0; i<playersWithKills.Count; i++)
+            {
+                if (playerKillNumber[i] > playerKillNumber[largestNumber]) { largestNumber = i; }
+            }
+            return (playersWithKills[largestNumber]);
+        }
+    }
+}
---
+++ MVP-Plugin/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EXILED;
+
+namespace MVP
+{
+    public class Plugin : EXILED.Plugin
+    {
+		public EventHandlers EventHandlers;
+
+		public override void OnEnable()
+		{
+			EventHandlers = new EventHandlers(this);
+			Events.PlayerDeathEvent += EventHandlers.OnPlayerDeath;
+			Events.RoundEndEvent += EventHandlers.OnRoundEnd;
+		}
+		public override void OnDisable()
+		{
+			Events.PlayerDeathEvent -= EventHandlers.OnPlayerDeath;
+			Events.RoundEndEvent -= EventHandlers.OnRoundEnd;
+		}
+		public override void OnReload()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string getName { get; } = "MVP by Luke-san";
+	}
+}
